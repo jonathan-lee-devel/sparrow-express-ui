3bc25855a50dfbf72e14d6a13d636fb63c3d02dd
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-06-08 00:57:28 +0100

    Corrected production API paths

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  MAIN_API_URL: 'https://api.sparrow.express',
-  RAW_API_URL: 'https://api.sparrow.express',
+  MAIN_API_URL: 'https://api.sparrow.express/api',
+  RAW_API_URL: 'https://api.sparrow.express/api',
 };
